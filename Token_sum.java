3f49d30eb9e12792eb8ce1c4b5b55c30b7ac4ad8
shubham kumar Gupta <37632080+shubhamiot@users.noreply.github.com>
2019-03-22 12:37:03 +0530

    Add files via upload

---
+++ Token_sum.java
@@ -0,0 +1,20 @@
+package com;
+
+import java.util.StringTokenizer;
+
+public class Token_sum 
+{
+	public static void main(String[] args) 
+	{
+		StringTokenizer st=new StringTokenizer("2 3 4 5");
+		System.out.println(st.countTokens());
+		long sum=0;
+		while(st.hasMoreTokens())
+		{
+			long i=Long.parseLong(st.nextToken());
+			System.out.println(i+"\n");
+			sum=sum+i;
+		}
+		System.out.println("the sum is "+sum);
+	}
+}
