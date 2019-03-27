c0d4ab56cb88f71a1eeccf13f4af39b86e920d11
shubham kumar Gupta <37632080+shubhamiot@users.noreply.github.com>
2019-03-28 01:00:10 +0530

    Add files via upload

---
+++ lab8/src/lab8/Thread1.java
@@ -0,0 +1,58 @@
+package lab8;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Thread1 extends Thread {
+	public static void main(String[] args) throws InterruptedException
+	{
+		Thread1 thread=new Thread1();
+		thread.start();
+	}
+	@Override
+	public void run()
+	{
+	try
+		{
+			File file=new File("D:\\Examples\\shubham.txt");
+	        StringBuilder sb=new StringBuilder("");
+	        BufferedReader br=new BufferedReader(new FileReader(file));
+	        String str = "";
+	        FileWriter fw=new FileWriter(new File("D:\\Examples\\target.txt"));
+	        while((str=br.readLine())!=null)
+	        {
+	        	sb.append(str);
+	        } 
+	        br.close();
+	        for (int i=0;i<sb.length() ;i++ ) 
+	        { 
+	        	if(i%10==0)
+	        	{
+	                System.out.println("\n Waiting for 5 seconds.... ");
+	                try
+		               {
+		               Thread.sleep(5000);
+		               }
+		            catch(InterruptedException ie)
+		               {
+	                    System.out.println("Smone Interrupted me..... ");
+		               }
+	        	}
+	        	fw.write(sb.charAt(i));
+	            fw.flush();
+	            //System.out.println("Contents from the file are : ");
+	            //System.out.print(sb.charAt(i)+"");   	
+	        }
+	        
+	        
+	        //System.out.println("Length of file is : "+ file.length());
+		}
+		catch(IOException e)
+		{
+			System.out.println("Can't read from the file currently... ");
+		}
+}
+}
---
+++ lab8/src/lab8/Timer.java
@@ -0,0 +1,28 @@
+package lab8;
+
+public class Timer extends Thread {
+	public static void main(String[] args) throws InterruptedException
+	{/*{
+		TimerClass th = new TimerClass();
+		th.start();
+		
+	}*/
+	/*@Override
+	public void run(){*/
+		for(int i=0;;i++)
+		{
+			System.out.print(i+"\r");
+			Thread.sleep(1000);
+				/*try
+				{
+					Thread.sleep(1000);
+				}
+				catch(InterruptedException e)
+				{
+					System.out.println("Interrupted");
+				}*/
+			//System.out.println(" ");
+		}
+	     //  System.out.println(Thread.currentThread().getName() + "-...Running..._");
+	}	
+}
